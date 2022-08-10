87e55b6f744f6cad3dd8cbdd1cabcea873e9b23a
Ayush Sharma <ThisIsAyush79@gmail.com>
2022-08-10 10:39:05 +0530

    test

---
+++ server.js
@@ -0,0 +1,13 @@
+var CronJob = require('cron').CronJob;
+console.log("start");
+var job = new CronJob(
+	'0 0 0 * * *',
+	function() {
+		console.log('You will see this message every second');
+	},
+	null,
+	true,
+	'America/Los_Angeles'
+);
+// Use this if the 4th param is default value(false)
+job.start()
